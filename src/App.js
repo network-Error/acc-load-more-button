b29eeadb3b977bf14bc53f49da5d2e946f355ad2
network-Error <iamimew@gmail.com>
2024-02-16 23:21:46 +0300

    init: start task

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { LoadMoreButton } from './components/LoadMoreButton';
+
+function App() {
+  return (
+    <div className="App">
+      <LoadMoreButton />
+    </div>
+  );
+}
+
+export default App;
